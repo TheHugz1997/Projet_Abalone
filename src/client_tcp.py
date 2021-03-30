646a875b8490040bc0384b60f231a5b3c441192e
alex210501 <alejandrobrb210501@gmail.com>
2021-03-30 16:30:10 +0200

    Bind the socket to a define port

--- src/client_tcp.py
+++ src/client_tcp.py
@@ -7,12 +7,16 @@ DEFAULT_PORT = 4704
 
 
 class ClientTCP:
-    def __init__(self, port=None):
+    def __init__(self, port=DEFAULT_PORT):
         self.__s = socket.socket()
-        self.__port = port if port is not None else DEFAULT_PORT
+        self.__port = port
 
     def connect_server(self):
         try:
+            # Bind the socket to the port choosed
+            self.__s.bind(('0.0.0.0', self.__port))
+
+            # Connection to the server
             self.__s.connect((SERVER_HOST, HOST_PORT))
         except socket.error as error:
             logging.critical(f"Socket error {error}")
@@ -20,7 +24,7 @@ class ClientTCP:
             return
 
         logging.info(f"Connected to the server. Host : {SERVER_HOST}, Port : {HOST_PORT}")
-
+        logging.info(f"Socket port : {self.__s.getsockname()}")
 
 
 if __name__ == "__main__":
